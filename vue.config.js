90e9de56d686ec1779e586fca401a31b890ec892
hao.zhang <735296233@qq.com>
2019-04-11 11:28:19 +0800

    :wrench: change build publicPath

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
+  publicPath: '.',
   configureWebpack: config => {
     if (isProduction)
       config.plugins = [...config.plugins, new BundleAnalyzerPlugin()]
